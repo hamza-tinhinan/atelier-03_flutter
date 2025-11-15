77dd7adacf1ee9fd3761c1f3035d0647dfe60a73
adel bennacer <adelbennacer46gmail.com>
2025-11-14 19:45:52 -0500

    ajouter de l'appbar

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: "Widgets de base",
+      home: Scaffold(
+       appBar: AppBar(
+        title: Text("Atelier 2"), 
+        centerTitle: true,
+         backgroundColor: Colors.blueAccent,
+       ),
+        body: Center(child: Text("Bienvenue dans cet atelier!")),
+      ),
+    );
+          child: Text('Hello World!');
+        
+      }
+    }
